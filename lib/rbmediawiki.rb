1ff9e763f9e62430ab7ff9780b4b9f8e56a7a289
Hugo Frappier <hugo@MacHugo.local>
2010-07-12 09:06:38 -0400

    Encapsulated in a module so we can use it with Rails

--- lib/rbmediawiki.rb
+++ lib/rbmediawiki.rb
@@ -3,7 +3,7 @@ require 'net/http'
 require 'rubygems'
 require 'xmlsimple'
 
-class Rbmediawiki
+module Rbmediawiki
     VERSION = '0.2.6.1'
     Dir["#{File.dirname(__FILE__)}/rbmediawiki/*.rb"].sort.each { |lib| require lib }
 end
